64391238ce1b8c20532cf5ac52c6de77f119c5da
Aloa0001 <45007602+Aloa0001@users.noreply.github.com>
2021-08-12 10:31:40 +0200

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,28 @@
+def testingWhenAndExpression() {
+  echo MAINTENANCE
+  echo BRANCH_NAME
+  echo 'Building ..'
+}
+
+def testingParameters() {
+  echo 'executing tests ...'
+}
+
+def testingEnvVarAndAddedEnvVar() {
+  echo "version: ${VERSION}"
+                echo "version: ${VERSION}"
+                echo "build: ${BUILD_ID}"
+                echo "ci: ${CI}"
+                echo "build number: ${BUILD_NUMBER}"
+                echo "job name: ${JOB_NAME}"
+                echo "jenkins url: ${JENKINS_URL}"
+                echo "build url: ${BUILD_URL}"
+                echo "job url: ${JOB_URL}"
+                echo "git url: ${GIT_URL}
+}
+
+def testingCredentialsUssage() {
+  echo "Credentials being used: ${CREDENTIALS}"
+}
+
+return this
