02e0a4a678f0e09bf894093e93d8b167f6329f00
Aloa0001 <45007602+Aloa0001@users.noreply.github.com>
2021-08-12 11:38:48 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -9,16 +9,15 @@ def testingParameters() {
 }
 
 def testingEnvVarAndAddedEnvVar() {
-  //echo "version: ${VERSION}"
   echo "version: ${params.VERSION}"
-  //echo "build: ${env.BUILD_ID}"
-  //echo "ci: ${env.CI}"
-  //echo "build number: ${env.BUILD_NUMBER}"
-  //echo "job name: ${env.JOB_NAME}"
-  //echo "jenkins url: ${env.JENKINS_URL}"
-  //echo "build url: ${env.BUILD_URL}"
-  //echo "job url: ${env.JOB_URL}"
-  //echo "git url: ${env.GIT_URL}
+  echo "build: ${env.BUILD_ID}"
+  echo "ci: ${env.CI}"
+  echo "build number: ${env.BUILD_NUMBER}"
+  echo "job name: ${env.JOB_NAME}"
+  echo "jenkins url: ${env.JENKINS_URL}"
+  echo "build url: ${env.BUILD_URL}"
+  echo "job url: ${env.JOB_URL}"
+  echo "git url: ${env.GIT_URL}"
 }
 
 def testingCredentialsUssage() {
